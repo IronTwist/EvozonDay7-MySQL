76eb121947d353b735eb24cba5e042afc68cc1e8
Fratean Razvan <47633722+IronTwist@users.noreply.github.com>
2021-07-16 13:18:30 +0300

    Add files via upload

--- MysqlPdo.php
+++ MysqlPdo.php
@@ -10,12 +10,13 @@ $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 echo "\n1.Alexei: ".PHP_EOL;
 //2 -> Select all distinct eployee names that work at London office,
-//and show me their names prefixed with Londono. The result column name should ne called employees
+//and show me their names prefixed with London. The result column name should ne called employees
 
 try {
     $sql = 'SELECT DISTINCT concat(\'London: \',firstName) AS \'Employees\' 
-            FROM employees 
-            INNER JOIN offices 
+            FROM employees E
+            INNER JOIN offices O
+            ON E.officeCode = O.officeCode
             WHERE city=\'London\'';
 
     echo '| Employees |' . PHP_EOL;
@@ -30,17 +31,24 @@ try {
 
 echo "\n2.Michael: ".PHP_EOL;
 //7 -> Show top 5 employees by sales
+//SELECT sum(P.amount)  FROM customers as C INNER JOIN payments as P ON P.customerNumber = C.customerNumber  INNER JOIN employees as E  ON C.salesRepEmployeeNumber = E.employeeNumber GROUP BY E.employeeNumber LIMIT 5;
+
 try {
-    $sql = 'SELECT lastName, firstName, amount 
-            FROM employees 
-            INNER JOIN customers 
-            INNER JOIN payments 
-            ORDER BY amount DESC limit 5';
+    $sql = 'SELECT E.employeeNumber, E.lastName, E.firstName, 
+            SUM(P.amount) AS Sales  
+            FROM customers as C 
+            INNER JOIN payments as P ON P.customerNumber = C.customerNumber  
+            INNER JOIN employees as E  ON C.salesRepEmployeeNumber = E.employeeNumber 
+            GROUP BY E.employeeNumber 
+            ORDER BY Sales DESC LIMIT 5;';
 
     echo 'lastName | firstName | amount'.PHP_EOL;
 
     foreach ($pdo->query($sql) as $row) {
-        print $row['lastName'].' '.$row['firstName'].' '.$row['amount'].PHP_EOL;
+        print $row['employeeNumber'].' '.
+            $row['lastName'].' '.
+            $row['firstName'].' '.
+            $row['Sales'].PHP_EOL;
     }
 }catch (Exception $e){
     echo 'Looks like you have an error, better check it: '. $e->getMessage();
@@ -55,11 +63,12 @@ try {
     $sql = 'SELECT contactFirstName, contactLastName, orderNumber, status, comments 
             FROM customers 
             INNER JOIN orders 
-            WHERE contactFirstName 
+            ON customers.customerNumber = orders.customerNumber
+            WHERE customers.contactFirstName
             LIKE \'T%\' 
             AND comments IS NOT NULL';
 
-    echo 'contactFirstName | contactLastName | orderNumber | status | comments'.PHP_EOL;
+    echo ' firstName | contactLastName | orderNumber | status | comments'.PHP_EOL;
 
     foreach ($pdo->query($sql) as $row) {
         print $row['contactFirstName'].' '.
